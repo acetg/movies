08bde7bb784893c83fc901f61774cd201ec25d61
acetg <officialganeshbhurkunde@gmail.com>
2025-02-14 10:28:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,11 +68,11 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://graph.org/file/7da9bafd3628ba059c884-44f42a5e5e285a4d46.jpg https://i.ibb.co/DQ35zLZ/image.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
-REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
+START_IMG = (environ.get('START_IMG', 'https://graph.org/file/7da9bafd3628ba059c884-44f42a5e5e285a4d46.jpg')).split()
+FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://graph.org/file/7da9bafd3628ba059c884-44f42a5e5e285a4d46.jpg')
+REFER_PICS = (environ.get("REFER_PICS", "https://graph.org/file/7da9bafd3628ba059c884-44f42a5e5e285a4d46.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/316ea60c9546cc9953294-84bf73787344c3ed7d.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/7da9bafd3628ba059c884-44f42a5e5e285a4d46.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
